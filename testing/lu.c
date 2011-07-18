de9dade37a35791692a6ed48157128db5d380ee5
Alexandre Lymberopoulos <lymber@ime.usp.br>
2011-07-18 20:36:04 -0300

    Experiments on LU splitting.

--- testing/lu.c
+++ testing/lu.c
@@ -5,84 +5,65 @@
 /* as entradas de uma matriz de floats com m linhas e n colunas. */
 void aloca_matriz(int m, int n, float ***M);
 
+/* Recebe inteiros m e n e **X, representando uma matriz m x n */
+/* Copia X em Y */
+void copia_matriz(int m, int n, float **X, float **Y);
+
 /* Recebe inteiros m e n e imprime o bloco [0..m]x[0..n] */
 /* de **M */
 void imprime_matriz(int m, int n, float **M);
 
-/* Recebe um inteiro m e um ponteiro para **M e libera a memória */
+/* Recebe um inteiro m e um ponteiro para **M e libera a memoria */
 /* alocada por **M "linha a linha" */
 void libera(int m, float ***M);
 
-/* Recebe inteiros m, n e l, **M e **N representando matrizes m por n e n por l  */
+/* Recebe inteiros m, n e l, **M e **N representando matrizes m x n e n x l  */
 /* respectivamente e um ponteiro para **P. Devolve em **P o produto das matrizes M e N */
 void multiplica(int m, int n, int l, float **M, float **N, float **P);
 
-/* Recebe inteiros m e n e **M, representando uma matriz m por n. */
-/* Devolve matrizes L e U, respectivamente triangulares inferior e */
-/* superior tais que M=LU. */
-void lu(int m, int n, float **M, float **L, float **U);
+/* Recebe inteiro n, floats k1 e k2 e vetores u[0..n] e v[0..n]. */
+/* Devolve em w a combinacao linear k1*u+k2*v */
+void comblin(int n, float k1, float k2, float u[], float v[], float w[]);
+
+/* Recebe inteiro n e **M, representando uma matriz n x n. */
+/* Devolve matrizes L e U, respectivamente triangulares inferior (n x n) e */
+/* superior (n x n) tais que M=LU. */
+void lu(int n, float **M, float **L, float **U);
 
 int main(void)
 {
 
-	int m, n, k, l;		/* linhas e colunas da matriz */
-	int i, j;		/* indices para linhas e colunas */
-	float **M, **L, **U;	/* guarda as entradas das matrizes M, N e P */
+	int i, j;
+	int n;		/* linhas e colunas da matriz */
+	float **M, **L, **U;	/* guarda as entradas das matrizes M, L e U */
 
-	printf("\nInforme o número de linhas da matriz M: ");
-	scanf("%d", &m);
-	printf("Informe o número de colunas da matriz M: ");
+	printf("\nInforme o nÃºmero de linhas e colunas da matriz M: ");
 	scanf("%d", &n);
-	aloca_matriz(m, n, &M);
+	aloca_matriz(n, n, &M);
+	aloca_matriz(n, n, &L);
+	aloca_matriz(n, n, &U);
 
-	printf("\nInforme o número de linhas da matriz L: ");
-	scanf("%d", &k);
-	printf("Informe o número de colunas da matriz L: ");
-	scanf("%d", &l);
-	aloca_matriz(k, l, &L);
-
-
-	for (i = 0; i < m; i++) {
-		for (j = 0; j < n; j++) {
-			M[i][j] = i + j;
-		}
+	for (i = 0; i < n; i++){
+		for (j = 0; j < n; j++){scanf("%f",&M[i][j]);}
 	}
 
-	for (i = 0; i < k; i++) {
-		for (j = 0; j < l; j++) {
-			L[i][j] = 2*i+j;
-		}
-	}
+	/* M[0][0]=2;M[0][1]=1;M[0][2]=1; */
+	/* M[1][0]=4;M[1][1]=-6;M[1][2]=0; */
+	/* M[2][0]=-2;M[2][1]=7;M[2][2]=2; */
 
-	for (i = 0; i < m; i++) {
-		M[i][2] = 0;
-	}
+	imprime_matriz(n, n, M);
 
-	imprime_matriz(m, n, M);
-	imprime_matriz(k, l, L);
+	lu(n, M, L, U);
 
-	if (n == k) {
-		aloca_matriz(m, l, &U);
-		multiplica(m, n, l, M, L, U);
-		printf("O produto de M e L é:\n");
-		imprime_matriz(m, l, U);
-	} else {
-		printf
-		    ("Número de colunas de M e linhas de L de tamanho diferentes, não é possível fazer o produto.\n");
-		printf("Desalocando a matriz M:\n");
-		libera(m, &M);
-		printf("Desalocando a matriz L:\n");
-		libera(k, &L);
-		printf("Saindo do programa...\n");
-		return 0;
-	}
+	printf("M=L*U:\n");
+	multiplica(n, n, n , L, U, M);
+	imprime_matriz(n, n, M);
 
-	printf("Desalocando a matriz M:\n");
-	libera(m, &M);
-	printf("Desalocando a matriz L:\n");
-	libera(k, &L);
-	printf("Desalocando a matriz U:\n");
-	libera(m, &U);
+	printf("Desalocando as matrizes envolvidas...");
+	libera(n, &M);
+	libera(n, &L);
+	libera(n, &U);
+	printf("Pronto!\n");
 	return 0;
 }
 
@@ -95,17 +76,28 @@ void aloca_matriz(int m, int n, float ***M)
 	*M = malloc(m * sizeof(float *));
 	if (*M == NULL) {
 		fprintf(stderr,
-			"Não há memória disponível para alocar a matriz\n");
+			"NÃ£o hÃ¡ memÃ³ria disponÃ­vel para alocar a matriz\n");
 		exit(EXIT_FAILURE);
 	}
 	for (i = 0; i < m; i++) {
 		(*M)[i] = malloc(n * sizeof(float));
 		if ((*M)[i] == NULL) {
 			fprintf(stderr,
-				"Não há memória disponível para alocar a linha %i da matriz\n",
+				"NÃ£o hÃ¡ memÃ³ria disponÃ­vel para alocar a linha %i da matriz\n",
 				i);
 			exit(EXIT_FAILURE);
 		}
+	}
+}
+
+/* ---------------------------------------------------------------------- */
+
+
+void copia_matriz(int m, int n, float **X, float **Y)
+{
+	int i, j;
+	for(i = 0; i < m; i++) {
+		for (j = 0; j < n; j++){Y[i][j]=X[i][j];}
 	}
 }
 
@@ -117,7 +109,6 @@ void libera(int m, float ***M)
 	int i;
 	for (i = 0; i < m; i++) {
 		free((*M)[i]);
-		printf("Linha %d desalocada\n", i);
 	}
 	free(*M);
 }
@@ -130,7 +121,7 @@ void imprime_matriz(int m, int n, float **M)
 	int i, j /* indices para percorrer a matriz */ ;
 	for (i = 0; i < m; i++) {
 		for (j = 0; j < n; j++)
-			printf("%.0f ", M[i][j]);
+			printf("%.4f\t", M[i][j]);
 		printf("\n");
 	}
 	printf("\n");
@@ -143,20 +134,41 @@ void multiplica(int m, int n, int l, float **M, float **N, float **P)
 
 	int i, j;		/* indices para percorrer a matriz P */
 	int k;			/* indice para percorrer as colunas de M e linhas de N */
-	for (i = 0; i < m; i++)
+	for (i = 0; i < m; i++) {
 		for (j = 0; j < l; j++) {
 			P[i][j] = 0;
-			for (k = 0; k < n; k++)
-				P[i][j] = P[i][j] + M[i][k] * N[k][j];
+			for (k = 0; k < n; k++) {P[i][j] = P[i][j] + M[i][k] * N[k][j];}
 		}
+	}
 }
 
 /* ---------------------------------------------------------------------- */
 
-void lu(int m, int n, float **M, float **L, float **U)
+void comblin(int n, float k1, float k2, float u[], float v[], float w[])
+{
+	int i;
+	for (i = 0; i < n; i++) {w[i] = k1 * u[i] + k2 * v[i];};
+}
+
+/* ---------------------------------------------------------------------- */
+
+void lu(int n, float **M, float **L, float **U)
 {
 	int i, j;		/* indices que percorrem a matriz M */
+	copia_matriz(n, n, M, U);
+	for (j = 0; j < n; j++){L[j][j]=1;}
+	printf("Passos da fatoraÃ§Ã£o:\n");
+	for (j = 0; j < n-1; j++) {
+		for (i = j+1; i < n; i++) {
+			L[i][j]=U[i][j]/U[j][j];
+			comblin(n, -L[i][j], 1, U[j], U[i], U[i]);
+			imprime_matriz(n, n, U);
+		}
+	}
+	printf("Matriz L:\n");
+	imprime_matriz(n, n, L);
 
+	printf("FatoraÃ§Ã£o concluÃ­da!\n");
 }
 
 /* ---------------------------------------------------------------------- */
