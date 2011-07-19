6add10e408a18b88c1f5ddf5365a528c60cc78a0
Alexandre Lymberopoulos <lymber@ime.usp.br>
2011-07-19 12:27:12 -0300

    Fixed data generation for warmup
    random creates two files: data-array containing one million poisition array of integers
    to be sorted by quicksort and data-matrix containing a 1000x1000 matrix of integers do
    be splitted by LU algorithm.

--- testing/random.c
+++ testing/random.c
@@ -1,21 +1,50 @@
 #include<stdio.h>
+#include<limits.h>
 #include<stdlib.h>
 #include<time.h>
 
 #define vecsize 1000000
+#define matrixsize 1000
+
+#define BACKSPACE 8  /* ^H */
+#define SPACE     ' '
+#define NEWLINE   '\n'
 
 int main()
 {
-  int i=0;
-  int v[vecsize];
+	int i, j, soma;
+	int m[matrixsize][matrixsize];
+	FILE *array, *matrix;
 
-  srand(time(NULL));
+	srand(time(NULL));
 
-  for (i=0; i<=vecsize-1; ++i){
-	  v[i]=rand();
-  }
-  for (i=0; i<=vecsize-1; ++i){
-  	  printf("%d\n",v[i]);
-  }
-  return 0;
+	printf("Generating random array of integers with %d positions...", vecsize);
+	array = fopen ("./data-array", "w");
+	for (i = 0; i < vecsize-1; i++){fprintf(array, "%d", rand()); putc(SPACE, array);}
+	fprintf(array, "%d", rand());
+	fclose(array);
+	printf(" Done!\n");
+
+	printf("Generating %d x %d random diagonally dominant matrix...", matrixsize, matrixsize);
+	for (i = 0; i < matrixsize; i++){
+		for (j = 0; j < matrixsize; j++){
+			if (j != i) {m[i][j] = INT_MAX/matrixsize * (double) rand () / ((double) RAND_MAX + 1);}
+			else{m[i][i] = 0;}
+		}
+	}
+	for (i = 0; i < matrixsize; i++){
+		soma=0;
+		for (j = 0; j < matrixsize; j++){soma = soma + abs(m[i][j]);}
+		m[i][i] = soma;
+	}
+	matrix = fopen ("./data-matrix", "w");
+	for (i = 0; i < matrixsize; i++){
+		for (j = 0; j<matrixsize-1; j++){
+			fprintf(matrix, "%d", m[i][j]); putc(SPACE, matrix);}
+		fprintf(matrix, "%d", m[i][j]); putc(NEWLINE, matrix);
+	}
+	fclose(matrix);
+	printf(" Done!\n");
+
+	return 0;
 }
