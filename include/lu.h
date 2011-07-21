8ec78acb39e4700adde75fcbe08f4dcc179ec880
Alexandre Lymberopoulos <lymber@ime.usp.br>
2011-07-21 19:54:47 -0300

    Translation to english of documentation

--- include/lu.h
+++ include/lu.h
@@ -1,31 +1,29 @@
 #ifndef LU_H
 #define LU_H
-/* Recebe inteiros m e n e um ponteiro para **M e armazena em **M  */
-/* as entradas de uma matriz de floats com m linhas e n colunas. */
+/* Takes ints m and n and a pointer to **M storing in **M  */
+/* the entries of a float matrix with m rows and n columns. */
 void aloca_matriz(int m, int n, float ***M);
 
-/* Recebe inteiros m e n e **X, representando uma matriz m x n */
-/* Copia X em Y */
+/* Takes ints m and n, **X **Y, treated as an m x n matrices. */
+/* Copies X to Y */
 void copia_matriz(int m, int n, float **X, float **Y);
 
-/* Recebe inteiros m e n e imprime o bloco [0..m]x[0..n] */
-/* de **M */
+/* Takes ints m and n and prints the block [0..m]x[0..n] of **M. */
 void imprime_matriz(int m, int n, float **M);
 
-/* Recebe inteiros m, n e l, **M e **N representando matrizes m x n e n x l  */
-/* respectivamente e um ponteiro para **P. Devolve em **P o produto das matrizes M e N */
+/* Takes ints m, n and l, matrices **M and **N m x n and n x l  */
+/* respectively and a pointer to **P. Stores in **P the product of M and N. */
 void multiplica(int m, int n, int l, float **M, float **N, float **P);
 
-/* Recebe um inteiro m e um ponteiro para **M e libera a memoria */
-/* alocada por **M "linha a linha" */
+/* Takes an int m and a pointer to **M freeing allocated memory to it */
+/* "row by row". */
 void libera(int m, float ***M);
 
-/* Recebe inteiro n, floats k1 e k2 e vetores u[0..n] e v[0..n]. */
-/* Devolve em w a combinacao linear k1*u+k2*v */
+/* Takes an int n, floats k1 and k2 e arrays u[0..n] and v[0..n]. */
+/* Stores in w the linear combination  k1*u+k2*v. */
 void comblin(int n, float k1, float k2, float u[], float v[], float w[]);
 
-/* Recebe inteiro n e **M, representando uma matriz n x n. */
-/* Devolve matrizes L e U, respectivamente triangulares inferior (n x n) e */
-/* superior (n x n) tais que M=LU. */
+/* Takes an int n and **M, a matrix n x n. Stores, respectively, in **L and */
+/* **U  an inferior and superior square matrices such that M=LU. */
 void lu(int n, float **M, float **L, float **U);
 #endif
