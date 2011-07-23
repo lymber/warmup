4924b55e25a619fd27e64a3cad67da74c2b3a217
Alexandre Lymberopoulos <lymber@ime.usp.br>
2011-07-23 09:20:21 -0300

    Translation of documentation to english

--- include/quicksort.h
+++ include/quicksort.h
@@ -1,6 +1,6 @@
 #ifndef QUICKSORT_H
 #define QUICKSORT_H
-/* Recebe vetor de inteiros v[p..r] com p <= r. Rearranja os elementos do vetor */
-/* em ordem crescente. */
+/* Takes an array v[p..r] of ints with p <= r. */
+/* Returns in *v a sorted permutation of its elements. */
 void quicksort(int v[], int p, int r);
 #endif
