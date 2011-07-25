2a19c7f3783a761dd8e4c3a6fb4d2223b43d2275
Alexandre Lymberopoulos <lymber@ime.usp.br>
2011-07-25 11:55:40 -0300

    Removed statis functions from header
    Functions matrix_copy and lin_comb don't need to be public.

--- include/lu.h
+++ include/lu.h
@@ -2,26 +2,18 @@
 #define LU_H
 /* Takes ints m and n and a pointer to **M storing in **M  */
 /* the entries of a float matrix with m rows and n columns. */
-void aloca_matriz(int m, int n, float ***M);
-
-/* Takes ints m and n, **X **Y, treated as an m x n matrices. */
-/* Copies X to Y */
-void copia_matriz(int m, int n, float **X, float **Y);
-
-/* Takes ints m and n and prints the block [0..m]x[0..n] of **M. */
-void imprime_matriz(int m, int n, float **M);
-
-/* Takes ints m, n and l, matrices **M and **N m x n and n x l  */
-/* respectively and a pointer to **P. Stores in **P the product of M and N. */
-void multiplica(int m, int n, int l, float **M, float **N, float **P);
+void matrix_alloc(int m, int n, float ***M);
 
 /* Takes an int m and a pointer to **M freeing allocated memory to it */
 /* "row by row". */
-void libera(int m, float ***M);
+void matrix_free(int m, float ***M);
 
-/* Takes an int n, floats k1 and k2 e arrays u[0..n] and v[0..n]. */
-/* Stores in w the linear combination  k1*u+k2*v. */
-void comblin(int n, float k1, float k2, float u[], float v[], float w[]);
+/* Takes ints m and n and prints the block [0..m]x[0..n] of **M. */
+void print_matrix(int m, int n, float **M);
+
+/* Takes ints m, n and l, matrices **M and **N m x n and n x l  */
+/* respectively and a pointer to **P. Stores in **P the product of M and N. */
+void matrix_multiply(int m, int n, int l, float **M, float **N, float **P);
 
 /* Takes an int n and **M, a matrix n x n. Stores, respectively, in **L and */
 /* **U  an inferior and superior square matrices such that M=LU. */
