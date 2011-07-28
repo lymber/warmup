95b60192d6366eafca527a138338fc354b1d4305
Alexandre Lymberopoulos <lymber@ime.usp.br>
2011-07-28 15:44:45 -0300

    Redesign of main function
    Now it takes arguments from command line specifying size of array and
    matrix to be used. This data is generated at execution time by gen_data
    function in auxiliary.{c,h}.
    
    There is also some basic error treatment argument that should be
    improved, specially the logic expression in the first if statement. It
    seems to produce desired results, but produces a segmentation fault when
    less then 3 arguments are passed to the program. Maybe logical operators
    are not associative in C and it has to evaluate every logic expression
    inside if statement. If so it tries to access invalid position in
    argv[]. Suggestions?

--- src/main.c
+++ src/main.c
@@ -5,68 +5,47 @@
 #include "../include/lu.h"
 #include "../include/auxiliary.h"
 
-#define vecsize 1000000
-#define matrixsize 1000
-
-int main(void)
+int main(int argc, char *argv[])
 {
 
-	FILE *array, *matrix;
-	int i = 0, j = 0, *v;
+	int vecsize, matrixsize;
+	int *v;
 	float **M, **L, **U;
 
-	v = mallocX(vecsize * sizeof(int *));
+	vecsize = atoi(argv[1]);
+	matrixsize = atoi(argv[2]);
 
-	printf("1) Sorting big array of integers test:\n");
-
-	printf("\t Reading data from file...");
-	array = fopen("./test/data-array", "r");
-	if (array == NULL) {
-		printf(" File not found!\n");
+	if ( (argc < 3) || (vecsize < 0) || (matrixsize < 0) ) {
+		printf("Usage: %s <non-negative array size> <non-negative matrix size>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	while ((i < vecsize) && (!feof(array))) {
-		fscanf(array, "%10d", &v[i]);
-		i++;
+	else {
+		printf("Allocating memory...\n");
+		v = mallocX(vecsize * sizeof(int *));
+		matrix_alloc(matrixsize, matrixsize, &M);
+		matrix_alloc(matrixsize, matrixsize, &L);
+		matrix_alloc(matrixsize, matrixsize, &U);
+		printf("Done!\n");
+
+		gen_data(vecsize, matrixsize, v, M);
+
+		printf("1) Sorting big array of integers:\n");
+
+		printf("\tSorting array of %d integers using quicksort...\n", vecsize);
+		quicksort(v, 0, vecsize - 1);
+		printf("\tDone!\n");
+
+		free(v);
+
+		printf("2) LU factorization of big square matrix:\n");
+
+		printf("\tFactorizing %d x %d matrix...\n", matrixsize, matrixsize);
+		lu(matrixsize, M, L, U);
+		printf("\tDone!\n");
+
+		matrix_free(matrixsize, &M);
+		matrix_free(matrixsize, &L);
+		matrix_free(matrixsize, &U);
 	}
-	printf(" Done!\n");
-	fclose(array);
-
-	printf("\t Sorting array of %d integers using quicksort...", vecsize);
-	quicksort(v, 0, vecsize - 1);
-	printf(" Done!\n");
-
-	free(v);
-
-	printf("2) LU factorizations of big square matrix:\n");
-
-	matrix_alloc(matrixsize, matrixsize, &M);
-	matrix_alloc(matrixsize, matrixsize, &L);
-	matrix_alloc(matrixsize, matrixsize, &U);
-
-	printf("\t Reading data from file...");
-	matrix = fopen("./test/data-matrix", "r");
-	if (matrix == NULL) {
-		printf(" File not found!\n");
-		exit(EXIT_FAILURE);
-	}
-	while (!feof(matrix)) {
-		for (i = 0; i < matrixsize; i++) {
-			for (j = 0; j < matrixsize; j++) {
-				fscanf(matrix, "%20f", &M[i][j]);
-			}
-		}
-	}
-	printf(" Done!\n");
-	fclose(matrix);
-
-	printf("\t Factorizing %d x %d matrix...", matrixsize, matrixsize);
-	lu(matrixsize, M, L, U);
-	printf(" Done!\n");
-
-	matrix_free(matrixsize, &M);
-	matrix_free(matrixsize, &L);
-	matrix_free(matrixsize, &U);
-
 	return 0;
 }
