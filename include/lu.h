281294642f64ead3518f0d551ab18a528c860242
Alexandre Lymberopoulos <lymber@ime.usp.br>
2011-08-01 15:52:56 -0300

    Renamed these files
    Since that files contain more than just quicksort and lu algorithms they
    were renamed to array.{c,h} and matrix.{c,h} and now contain everything
    related to this kind of object.

--- include/lu.h
+++
@@ -1,7 +0,0 @@
-#ifndef LU_H
-#define LU_H
-
-/* Takes an int n and **M, a matrix n x n. Stores, respectively, in **L and */
-/* **U  an inferior and superior square matrices such that M=LU. */
-void lu(int n, float **M, float **L, float **U);
-#endif
--- include/quicksort.h
+++
@@ -1,6 +0,0 @@
-#ifndef QUICKSORT_H
-#define QUICKSORT_H
-/* Takes an array v[p..r] of ints with p <= r. */
-/* Returns in *v a sorted permutation of its elements. */
-void quicksort(int v[], int p, int r);
-#endif
